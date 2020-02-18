1e367bc5b42abc844f426ef85013b131dad49a19
tanvir-ome <tanvirome378@gmail.com>
2020-02-18 22:55:47 +0600

    CRUD Operation using LINQ

---
+++ DotNet-MVC-LINQ/Controllers/PersonController.cs
@@ -0,0 +1,77 @@
+﻿using DotNet_MVC_LINQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNet_MVC_LINQ.Controllers
+{
+    public class PersonController : Controller
+    {
+        PersonRepository repo = new PersonRepository();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            //var person = context.Persons.SingleOrDefault(x => x.Id == id);
+            return View(repo.Details(id));
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person p)
+        {
+            //context.Persons.InsertOnSubmit(p);
+            //context.SubmitChanges();
+            p = repo.Create(p);
+            return RedirectToAction("Details", new { id = p.id });
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            //var person = context.Persons.SingleOrDefault(x => x.Id == id);
+            return View(repo.Details(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Person p)
+        {
+            //var person = context.Persons.SingleOrDefault(x => x.Id == p.Id);
+            //person.Name = p.Name;
+            //person.Email = p.Email;
+            //context.SubmitChanges();
+            repo.Edit(p);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            //var person = context.Persons.SingleOrDefault(x => x.Id == id);
+            return View(repo.Details(id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmDelete(Person p)
+        {
+            //var person = context.Persons.SingleOrDefault(x => x.Id == p.Id);
+            //context.Persons.DeleteOnSubmit(person);
+            //context.SubmitChanges();
+            repo.Delete(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DotNet-MVC-LINQ/Models/PersonRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNet_MVC_LINQ.Models
+{
+    public class PersonRepository
+    {
+        PersonDbDataContext context = new PersonDbDataContext();
+        public List<Person> GetAll()
+        {
+            var list = context.Persons.ToList();
+            return list;
+        }
+
+        public Person Details(int id)
+        {
+            var person = context.Persons.SingleOrDefault(x => x.id == id);
+            return person;
+        }
+
+        public Person Edit(Person p)
+        {
+            var person = context.Persons.SingleOrDefault(x => x.id == p.id);
+            person.name = p.name;
+            person.email = p.email;
+            context.SubmitChanges();
+            return p;
+        }
+
+        public Person Create(Person p)
+        {
+            context.Persons.InsertOnSubmit(p);
+            context.SubmitChanges();
+            //int id = p.id;
+            return p;
+        }
+
+        public Person Delete(Person p)
+        {
+            var person = context.Persons.SingleOrDefault(x => x.id == p.id);
+            context.Persons.DeleteOnSubmit(person);
+            context.SubmitChanges();
+            return p;
+        }
+    }
+}
